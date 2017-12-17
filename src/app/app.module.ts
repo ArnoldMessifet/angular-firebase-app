87f5ff3af33c27ffff1217903cbad26f25ccfb8b
Arnold MESSIFET <am.frontdev@gmail.com>
2017-12-17 14:36:04 +0100

    Added basic components
    add navbar, home,  not found and contact components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
+import { RouterModule, Route } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
+
+const routes: Array<Route> = [
+  { path: 'contacts', component: ContactsComponent },
+  { path: 'dashboard', component: HomeComponent },
+  { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    ContactsComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      routes,
+      { enableTracing: true }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
